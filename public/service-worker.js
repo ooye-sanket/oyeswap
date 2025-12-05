2cf6085ac3aa16004a48af8fea1ce7c626b88629
Sanket Kalekar <kalekarsanket005@gmail.com>
2025-12-05 18:30:07 +0530

    Add service worker for asset caching
    Implement a service worker for caching assets.

---
+++ public/service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'oyeswap-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/media/favicon.svg',
+  '/media/logo.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
